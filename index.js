bf335a7c4f2c162c72112685430fba01379f6e0b
David <bitomule@gmail.com>
2018-04-17 19:51:56 +0200

    Test basics

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  const handlePullRequest = require('./lib/pull-request-update')
+  robot.on([
+    'pull_request.opened'
+  ], handlePullRequest)
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,42 @@
-// You can import your modules
-// const index = require('../index')
+// Requiring probot allows us to mock out a robot instance
+const {createRobot} = require('probot')
+// Requiring our app
+const app = require('..')
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
+function fixture (name, path) {
+  return {
+    event: name,
+    payload: require(path)
+  }
+}
+
+describe('probot-pr-template', () => {
+  let robot
+  let github
+  beforeEach(() => {
+    // Here we create a robot instance
+    robot = createRobot()
+    // Here we initialize the app on the robot instance
+    app(robot)
+    // This is an easy way to mock out the GitHub API
+    github = {
+      pullRequests: {
+        update: jest.fn().mockReturnValue(Promise.resolve({
+          data: [{ 'id': 1 }]
+        }))
+      }
+    }
+    // Passes the mocked out GitHub API into out robot instance
+    robot.auth = () => Promise.resolve(github)
+  })
+
+  describe('test events', () => {
+    it('when pull requests are opened content is updated', async () => {
+      const payload = fixture('pull_request', './fixtures/pull_request.opened')
+      // Simulates delivery of a payload
+      await robot.receive(payload)
+
+      expect(github.pullRequests.update).toHaveBeenCalled()
+    })
+  })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
